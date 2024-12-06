a9acb758a1bedab1411264ce4f5e38eed93ff9d9
Umut Enes Soğukpınar <usogukpinar0@gmail.com>
2024-12-07 01:11:26 +0300

    action lib added

---
+++ includes/actions.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umut <umut@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 01:08:12 by umut              #+#    #+#             */
+/*   Updated: 2024/12/07 01:10:26 by umut             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ACTIONS_H
+# define ACTIONS_H
+
+#include "libft.h"
+
+void	push(t_list **source, t_list **target);
+void	swap(t_list **list);
+void	rotate(t_list **list);
+void	reverse_rotate(t_list **list);
+
+# endif
