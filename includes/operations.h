1537f7a58c7a9f88eef6f4807e7072ab94da02a5
Umut Enes Soğukpınar <usogukpinar0@gmail.com>
2024-12-10 20:18:57 +0300

    headers added

---
+++ includes/operations.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umut <umut@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 20:08:38 by umut              #+#    #+#             */
+/*   Updated: 2024/12/10 20:12:44 by umut             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OPERATIONS_H
+# define OPERATIONS_H
+
+# include "actions.h"
+
+void	push_a(t_list **source, t_list **target);
+void	push_b(t_list **source, t_list **target);
+
+void	rotate_a(t_list **list);
+void	rotate_b(t_list **list);
+void	rotate_both(t_list **list_one, t_list **list_two);
+
+void	reverse_rotate_a(t_list **list);
+void	reverse_rotate_b(t_list **list);
+void	reverse_rotate_both(t_list **list_one, t_list **list_two);
+
+# endif
