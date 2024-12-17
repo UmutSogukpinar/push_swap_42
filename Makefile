f0ad70a40031f9b6218f717ca21551c4d82fefe1
Umut Enes Soğukpınar <usogukpinar0@gmail.com>
2024-12-17 19:38:57 +0300

    main added

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 NAME = push_swap
 
 ACT_DIR = srcs/actions
+CONT_DIR = srcs/controls
 LIB_DIR = ./libs
 
-SRCS = srcs/main.c
+SRCS = push_swap.c \
+		${CONT_DIR}/is_number.c
 OBJS = ${SRCS:.c=.o}
 
-CC = gcc
+CC = cc
 CFLAGS = -Wall -Wextra -Werror -Iincludes -Ilibs/my_libft -Ilibs/my_printf
 
 RM = rm -rf
@@ -19,11 +21,13 @@ ${NAME}: ${OBJS}
 	${CC} ${CFLAGS} ${OBJS} ${LIB_DIR}/my_libft/libft.a ${LIB_DIR}/my_printf/libftprintf.a -o ${NAME}
 
 clean: 
-	${MAKE} -C ./libs/my_libft fclean
-	${MAKE} -C ./libs/my_printf fclean
+	${MAKE} -C ${LIB_DIR}/my_libft clean
+	${MAKE} -C ${LIB_DIR}/my_printf clean
 	${RM} ${OBJS}
 
 fclean: clean
+	${MAKE} -C ${LIB_DIR}/my_libft fclean
+	${MAKE} -C ${LIB_DIR}/my_printf fclean
 	${RM} ${NAME}
 
 re: fclean all
---
+++ push_swap.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umut <umut@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 23:01:18 by umut              #+#    #+#             */
+/*   Updated: 2024/12/17 19:35:53 by umut             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+#include "controls.h"
+
+t_list	*init_stack(char **args)
+{
+	int		i;
+	int		*new_value;
+	t_list	*new_node;
+	t_list	*new_stack;
+
+	i = 0;
+	new_stack = NULL;
+	while (args[++i])
+	{
+		new_value = ft_create_int_ptr(args[i]);
+		if (!new_value)
+		{
+			ft_lstclear(&new_stack, free);
+			return (NULL);
+		}
+		new_node = ft_lstnew(new_value);
+		if (!new_node)
+		{
+			free(new_value);
+			ft_lstclear(&new_stack, free);
+			return (NULL);			
+		}
+		ft_lstadd_back(&new_stack, new_node);
+	}
+	return (new_stack);
+}
+
+int	main(int arg_number, char **args)
+{
+	t_list	*stack_a;
+	//t_list	*stack_b;
+
+	if (arg_number <= 1)
+		return (0);
+	stack_a = init_stack(args);
+	//stack_b = NULL;
+	if (!stack_a)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	ft_display_int_list(stack_a);
+}
