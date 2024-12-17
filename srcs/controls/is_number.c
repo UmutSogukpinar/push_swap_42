089f08f3514b78bc740e233f9cf6700fefad3118
Umut Enes Soğukpınar <usogukpinar0@gmail.com>
2024-12-17 18:11:08 +0300

    controls added

---
+++ srcs/controls/is_number.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_number.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umut <umut@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 23:16:22 by umut              #+#    #+#             */
+/*   Updated: 2024/12/17 18:09:44 by umut             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_is_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] != '\0')
+	{
+		if (!ft_isdigit((int) str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	*ft_create_int_ptr(char *str)
+{
+	int	*new_value;
+
+	new_value = malloc(sizeof(int));
+	if (!new_value)
+		return (NULL);
+	*new_value = ft_atoi(str);
+	return (new_value);
+}
