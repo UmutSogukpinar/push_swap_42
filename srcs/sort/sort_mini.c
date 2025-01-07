c4529df184ace70590b8d08ab450ad6194e5e641
Umut Enes Soğukpınar <usogukpinar0@gmail.com>
2025-01-07 13:53:55 +0300

    sort mini init

--- srcs/sort/sort_mini.c
+++ srcs/sort/sort_mini.c
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort.c                                             :+:      :+:    :+:   */
+/*   sort_mini.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umut <umut@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/07 13:38:43 by umut              #+#    #+#             */
-/*   Updated: 2025/01/07 13:51:35 by umut             ###   ########.fr       */
+/*   Created: 2025/01/07 13:52:16 by umut              #+#    #+#             */
+/*   Updated: 2025/01/07 13:53:40 by umut             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sort.h"
-#include "stack.h"
 
-void	sort(t_sort *main)
+// I stay there!!!!
+
+void	sort_for_two(t_sort *main)
 {
-	if (ft_lstsize(main -> stack_a) == 2)
-		sort_for_two(&(main -> stack_a));
-	else if (ft_lstsize(main -> stack_a) == 3)
-		sort_for_three(&(main -> stack_a));
-	else if (ft_lstsize(main -> stack_a) >= 4)
-		sort_default(main);
+	t_list	*head;
+	t_list	*tail;
+	int		head_value;
+	int		tail_value;
+
+	head = *stack;
+	tail = head -> next;
+	head_value = *(int *)head -> content;
+	tail_value = *(int *)tail -> content;
+	if (head_value <= tail_value)
+		return ;
+	swap_a(stack);
 }
